0e4a69d86f2801074124abe626beac7c8a6f2a0f
Monica  Karasek <mlkarasek@gmail.com>
2017-11-04 20:32:31 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,19 +1,8 @@
-
-module Concerns::Findable
-
-  def find_by_name(name)
-    all.map do |song|
-      if song.name == name
-        return song
-      end
-    end
+def find_by_name(name)
+    self.all.find{|song| song.name == name}
   end
 
   def find_or_create_by_name(name)
-    if self.find_by_name(name) == nil
-      self.create(name)
-    else
-      self.find_by_name(name)
+    self.find_by_name(name) || self.create(name)
   end
-end
 end
